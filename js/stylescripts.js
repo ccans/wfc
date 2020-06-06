b28ce756c523773fc26f9e61db5ff65b005711e2
ccans <55026208+ccans@users.noreply.github.com>
2020-06-06 12:17:49 -0500

    a

--- js/stylescripts.js
+++ js/stylescripts.js
@@ -47,13 +47,13 @@ $(function(){
     }
   });
 });
-
-var rn = Math.floor((Math.random() * 150) + 60);
-var rs = Math.floor((Math.random() * 11) + 4);
-	var t = new Trianglify({
- x_gradient: Trianglify.colorbrewer.Spectral[rs],
-    noiseIntensity: 0,
-    cellsize: rn
-});
-var pattern = t.generate(window.innerWidth, window.innerWidth+200);
-document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+//
+// var rn = Math.floor((Math.random() * 150) + 60);
+// var rs = Math.floor((Math.random() * 11) + 4);
+// 	var t = new Trianglify({
+//  x_gradient: Trianglify.colorbrewer.Spectral[rs],
+//     noiseIntensity: 0,
+//     cellsize: rn
+// });
+// var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+// document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
