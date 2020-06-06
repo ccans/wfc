d33771acdfca654f086bf7e16778851c54c7a9dd
ccans <55026208+ccans@users.noreply.github.com>
2020-06-05 23:05:35 -0500

    lmao

---
+++ js/stylescripts.js
@@ -0,0 +1,41 @@
+var droppedDown = false;
+
+function dropDown() {
+  if(droppedDown == false) {
+        $('.dropdown-navbar').stop();
+          curHeight =  $('.dropdown-navbar').height();
+          autoHeight = $(window).height(); - $('.header').height();
+        $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
+        $('body').addClass("unscroll");
+        $('.dropdown-navbar').show();
+        $('.dropdown-content').css("display", "block");
+        $('.header').addClass('dropdown');
+        droppedDown = true;
+    } else {
+      $('.dropdown-navbar').stop();
+      curHeight = $('.dropdown-navbar').height();
+      autoHeight = '0px';
+      $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
+      $('body').removeClass("unscroll");
+      setTimeout(function(){
+      $('.dropdown-content').css("display", "none");
+    }, 750);
+      droppedDown = false;
+      $('.header').removeClass("dropdown")
+
+    }
+}
+
+window.onload = function() {
+    $('.titleText').fadeIn(1000);
+};
+
+var rn = Math.floor((Math.random() * 150) + 60);
+var rs = Math.floor((Math.random() * 11) + 4);
+	var t = new Trianglify({
+ x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+});
+var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
